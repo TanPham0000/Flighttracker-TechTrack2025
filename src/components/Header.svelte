90672f102f08b71dd3062ad070050d4436299f7e
That's me <tan.pham2709@gmail.com>
2025-11-10 13:26:09 +0100

    init

---
+++ src/components/Header.svelte
@@ -0,0 +1,23 @@
+<style lang="scss">
+    header {
+        background-color: #1b6804;
+        padding: 1rem;
+        text-align: center;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    }
+
+    h1 {
+        margin: 0;
+        font-size: 1.5rem;
+        color: rgb(7, 131, 255);
+    }
+    p {
+        margin: 0.5rem 0 0;
+        color: #ffffff;
+    }
+</style>
+
+<header>
+    <h1>My lineplot</h1>
+    <p>wow look at this line it goes up</p>
+</header>
